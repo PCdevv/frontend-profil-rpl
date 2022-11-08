acbd8f14f6db1c21027185a059f3339052c8e7db
PCdevv <putericitradev@gmail.com>
2022-11-08 11:21:50 +0700

    fix about

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import React from 'react';
 import Button from './components/button';
 import Card from './components/card';
 import Navbar from './components/navbar';
-import logo from './logo.svg';
 import About from './components/about'
 import Kontak from './components/kontak';
 import Footer from './components/footer';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-      <Button/>
-      <Card/>
-      <Kontak/>
-      <About/>
-      <Footer/>
-      
-    </div>
+    <Router>
+      <div className="app">
+        <Navbar/>
+          <div className="content">
+            <Button/>
+            <Card/>
+            <Kontak/>
+            <About/>
+          </div>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
--- src/components/about.js
+++ src/components/about.js
@@ -1,21 +1,21 @@
 const About = () => {
-    return (
-        <div className="hero min-h-screen bg-white">
-  <div className="hero-content flex-col ">
-    <img src="https://placeimg.com/260/400/arch" className="max-w-sm shadow-2xl" />
-    <div>
-</div>
-</div>
-<div className="toast toast-center">
-  <div className="bg-white hover:bg-green alert alert-info">
-    <div>
-      <span>GW UDAH LELAH</span>
-    </div>
-  </div>
-  </div>
-  
-  </div>
-
+  return (
+      <div className="bg-white">
+          <div className="items-center text-center opacity-70 rounded-none rounded-tr-lg rounded-bl-lg card w-full shadow-2xl bg-green">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text font-bold text-5x1 text-black">About</span>
+                </label>
+              </div>
+            </div>
+          </div>
+        <div className="hero min-h-screen">
+        <div className="hero-content flex-col ">
+          <img src="https://placeimg.com/260/400/arch" className="max-w-sm shadow-2xl" />
+        </div>
+      </div>
+      </div>
     );
 }
 
